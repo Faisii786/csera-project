9db282fd45f19e0785a61c566616403de9056650
Faisal Aslam <Faisalaslam218@gmail.com>
2024-01-17 01:52:36 +0500

    csera app

--- lib/nayyer.dart
+++
@@ -1,2 +0,0 @@
-//gjgjhvghcvh
-//sasdafaf
---
+++ lib/screens/Dashboard.dart
@@ -0,0 +1,15 @@
+import 'package:csera_app/utility/colors.dart';
+import 'package:flutter/material.dart';
+
+class DashboardScreen extends StatelessWidget {
+  const DashboardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors().backgroundColors,
+      ),
+    );
+  }
+}
