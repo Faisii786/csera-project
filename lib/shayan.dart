5b29a6d26c6127b78e8de9096dc2511a2348b8ef
shayan5522 <shayanahmed743@gmail.com>
2024-01-16 08:28:24 -0800

    Add personal page

---
+++ lib/shayan.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShayanBhai extends StatefulWidget {
+  const ShayanBhai({super.key});
+
+  @override
+  State<ShayanBhai> createState() => _ShayanBhaiState();
+}
+
+class _ShayanBhaiState extends State<ShayanBhai> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
