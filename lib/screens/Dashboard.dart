68bbb1c3ef44b565d0c8c91fbec6583f6dd8bc65
syedtayab321 <tayabsajid5@gmail.com>
2024-01-17 22:04:45 -0800

    tayab

--- lib/screens/Dashboard.dart
+++ lib/screens/Dashboard.dart
@@ -3,7 +3,9 @@ import 'package:csera_app/utility/colors.dart';
 import 'package:csera_app/widgets/course_container.dart';
 import 'package:flutter/material.dart';
 import 'package:csera_app/widgets/lists.dart';
-
+import 'package:csera_app/widgets/coursel.dart';
+import 'package:csera_app/widgets/about_text.dart';
+import 'package:csera_app/widgets/logo_container.dart';
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
 
@@ -41,7 +43,7 @@ class _DashboardScreenState extends State<DashboardScreen>
             children: [
               DrawerHeader(
                 child: CircleAvatar(
-                  backgroundImage: AssetImage('assets/images/appbar_logo.jpg'),
+                  backgroundImage: AssetImage('assets/images/logo.png'),
                   radius: 60,
                 ),
               ),
@@ -140,118 +142,118 @@ class _DashboardScreenState extends State<DashboardScreen>
         ),
         backgroundColor: AppColors().BgColor,
         body: SingleChildScrollView(
-            child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text(
-                    "Course offered",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                  ),
-                  ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.deepPurple),
-                      onPressed: () {},
-                      child: const Text(
-                        "View All",
-                        style: TextStyle(color: Colors.white),
-                      ))
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              CourseCard(
-                  imagePath: "assets/images/amazon.png",
-                  title: 'Amazon',
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => cyberdetail()));
-                  }),
-              SizedBox(
-                height: 10,
-              ),
-              CourseCard(
-                  imagePath: "assets/images/cyber.jpg",
-                  title: 'Cyber Security',
-                  onTap: () {}),
-              SizedBox(
-                height: 10,
-              ),
-              CourseCard(
-                  imagePath: "assets/images/ai.jpeg",
-                  title: 'Artificial Intellegence',
-                  onTap: () {}),
-              SizedBox(
-                height: 10,
-              ),
-              CourseCard(
-                  imagePath: "assets/images/cloud.jpg",
-                  title: 'Cloud Computing',
-                  onTap: () {}),
-              SizedBox(
-                height: 10,
-              ),
-              CourseCard(
-                  imagePath: "assets/images/networking.jpeg",
-                  title: 'Networking',
-                  onTap: () {}),
-              SizedBox(
-                height: 10,
-              ),
-              CourseCard(
-                  imagePath: "assets/images/python.jpeg",
-                  title: 'Python',
-                  onTap: () {}),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text(
-                    "Training Programs",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                  ),
-                  ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.deepPurple),
-                      onPressed: () {},
-                      child: const Text(
-                        "View All",
-                        style: TextStyle(color: Colors.white),
-                      )),
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              CourseCard(
-                  imagePath: "assets/images/python.jpeg",
-                  title: 'CCNA Certification',
-                  onTap: () {}),
-              SizedBox(
-                height: 10,
-              ),
-              CourseCard(
-                  imagePath: "assets/images/python.jpeg",
-                  title: 'CCNP Certification',
-                  onTap: () {}),
-              SizedBox(
-                height: 10,
-              ),
-              CourseCard(
-                  imagePath: "assets/images/python.jpeg",
-                  title: 'Az 104',
-                  onTap: () {}),
-              SizedBox(
-                height: 10,
-              ),
-            ],
-          ),
-        )));
+            child: Column(
+              children: [
+                ImageCarousel(),
+                logo_container(),
+                ClickMoreSection(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      "Course offered",
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                    ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.deepPurple),
+                        onPressed: () {},
+                        child: const Text(
+                          "View All",
+                          style: TextStyle(color: Colors.white),
+                        ))
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                CourseCard(
+                    imagePath: "assets/images/amazon.png",
+                    title: 'Amazon',
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => cyberdetail()));
+                    }),
+                SizedBox(
+                  height: 10,
+                ),
+                CourseCard(
+                    imagePath: "assets/images/cyber.jpg",
+                    title: 'Cyber Security',
+                    onTap: () {}),
+                SizedBox(
+                  height: 10,
+                ),
+                CourseCard(
+                    imagePath: "assets/images/ai.jpeg",
+                    title: 'Artificial Intellegence',
+                    onTap: () {}),
+                SizedBox(
+                  height: 10,
+                ),
+                CourseCard(
+                    imagePath: "assets/images/cloud.jpg",
+                    title: 'Cloud Computing',
+                    onTap: () {}),
+                SizedBox(
+                  height: 10,
+                ),
+                CourseCard(
+                    imagePath: "assets/images/networking.jpeg",
+                    title: 'Networking',
+                    onTap: () {}),
+                SizedBox(
+                  height: 10,
+                ),
+                CourseCard(
+                    imagePath: "assets/images/python.jpeg",
+                    title: 'Python',
+                    onTap: () {}),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      "Training Programs",
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                    ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.deepPurple),
+                        onPressed: () {},
+                        child: const Text(
+                          "View All",
+                          style: TextStyle(color: Colors.white),
+                        )),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                CourseCard(
+                    imagePath: "assets/images/python.jpeg",
+                    title: 'CCNA Certification',
+                    onTap: () {}),
+                SizedBox(
+                  height: 10,
+                ),
+                CourseCard(
+                    imagePath: "assets/images/python.jpeg",
+                    title: 'CCNP Certification',
+                    onTap: () {}),
+                SizedBox(
+                  height: 10,
+                ),
+                CourseCard(
+                    imagePath: "assets/images/python.jpeg",
+                    title: 'Az 104',
+                    onTap: () {}),
+                SizedBox(
+                  height: 10,
+                ),
+              ],
+            )));
   }
 }
