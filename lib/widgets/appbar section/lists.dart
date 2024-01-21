81185cdd58e79abe2971344c4b9ef5c6e499b3a1
shayan5522 <shayanahmed743@gmail.com>
2024-01-20 23:20:04 -0800

    ssaa

--- lib/widgets/appbar section/lists.dart
+++ lib/widgets/appbar section/lists.dart
@@ -1,4 +1,9 @@
+import 'package:csera_app/screens/Dashboard.dart';
+import 'package:csera_app/screens/about%20section/whatisCsera.dart';
 import 'package:csera_app/widgets/contact%20section/contact%20us.dart';
+import 'package:csera_app/widgets/course%20section/course_container.dart';
+import 'package:csera_app/widgets/course%20section/courses_cards.dart';
+import 'package:csera_app/widgets/course%20section/training_cards.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 class lists extends StatelessWidget {
@@ -17,16 +22,16 @@ class lists extends StatelessWidget {
       onTap:() {
         if(value=="1")
           {
-
+        Get.to(DashboardScreen());
           }
         else if(value=="2"){
-
+        Get.to(whatisCsera());
         }
         else if(value=="3"){
-
+          Get.to(training_cards());
         }
         else if(value=="4"){
-
+      Get.to(courses_cards());
         }
         else if(value=="5"){
           Get.to(contact_us());
