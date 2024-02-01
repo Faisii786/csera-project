4fa559535a70ac604e5066f960db7d7ef5adce02
syedtayab321 <tayabsajid5@gmail.com>
2024-02-01 11:56:33 -0800

    tayab

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:csera_app/screens/Dashboard.dart';
+import 'package:csera_app/screens/MainScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: DashboardScreen(),
+      home: Main_screen(),
     );
   }
 }
